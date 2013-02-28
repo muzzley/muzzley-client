b8ae37a558e62c1b28ecc8d3ac34be8a971579cd
Bruno Barreto <bruno.aka.necker@gmail.com>
2013-02-28 11:25:15 +0000

    new tests and some documentation

---
+++ README.md
@@ -0,0 +1,23 @@
+# muzzley-sdk-js
+
+**Version: 0.1.0 Draft**
+
+This document describes how to use the muzzley javascript SDK.
+
+## Installing
+
+### On the browser
+
+### Browserify
+
+### Ender support
+soon.
+
+### Node.js
+
+
+## API Documentation
+
+
+## Modify and test
+
--- compile.js
+++ compile.js
@@ -1,15 +1,13 @@
 var fs = require('fs');
 var browserify = require('browserify');
 
-var js = browserify(['./vendor/sockjs.js']);
-js.require('./lib/');
-js.require('./lib/remoteCalls.js');
-js.require('./lib/rpcManager.js');
+var js = browserify();
+js.require('./lib/', {expose:'muzzley-sdk-js'});
 
 js.add('./lib/browser.js');
 
 js.bundle({watch: true, filter : require('uglify-js') }, function(err, file){
-  fs.writeFile(__dirname + '/bin/muzzley-sdk.js', file, function (err) {
+  fs.writeFile(__dirname + '/dist/muzzley-sdk.js', file, function (err) {
     if (err) throw err;
     console.log('It\'s saved! and ready to use on: ' + __dirname + '/html/js/muzzley-sdk.js');
   });
