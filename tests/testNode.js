b42eab08638838cac5b9b3adda65044010261b97
Bruno Barreto <bruno.aka.necker@gmail.com>
2013-03-08 17:32:28 +0000

    nodejs lib tests (createActivity)

--- tests/testNode.js
+++ tests/testNode.js
@@ -5,4 +5,15 @@ muzzley.createActivity('asd', function(err, activity){
 
   console.log(activity);
   console.log('connectou');
+
+  activity.on('participantJoin', function(participant){
+    console.log(participant);
+    participant.changeWidget('gamepad', function (err) {
+      console.log('changeWidget');
+      participant.on('action', function (action) {
+        // Action object represents the participants interaction
+        console.log(action);
+      });
+    });
+  });
 });
