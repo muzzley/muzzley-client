07eb4ec8750f2da94163ea569090206a0cae3b00
BruNeX <mail@brunex.name>
2013-03-14 18:03:44 +0000

    Tests update to test error handling

--- tests/testNode.js
+++ tests/testNode.js
@@ -2,7 +2,8 @@ var muzzley = require('../lib/node-dist.js');
 
 
 console.log('##Activity: creating activity');
-muzzley.createActivity('muzzlionaire', function(err, activity){
+muzzley.createActivity('muzzliodnaire', function(err, activity){
+  if (err) return console.log("err: " + err );
 
   muzzley.joinActivity('muzdev', activity.activityId, function(err, paticipant){
 
--- tests/testNodeUser.js
+++ tests/testNodeUser.js
@@ -1,8 +1,8 @@
 var muzzley = require('../lib/node-dist.js');
 
 
-muzzley.joinActivity('muzdev','1f5016', function(err, paticipant){
-
+muzzley.joinActivity('guest','cd7679', function(err, paticipant){
+  if (err) return console.log('err:' +  err);
   //console.log(paticipant);
   console.log('joinedActivity');
   paticipant.on('changeWidget', function(widget){
