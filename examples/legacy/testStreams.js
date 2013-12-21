fe6d53c83d06855bedc6ed232126215e1ad7bca7
tiagoalves <tiago.alves@muzzley.com>
2013-12-21 23:06:56 +0000

    Removed legacy examples.

--- examples/legacy/testStreams.js
+++
@@ -1,7 +0,0 @@
-var muzzley = require('../lib/stream');
-
-var muzzleyConnection = new muzzley();
-
-muzzleyConnection.createActivity('your-app-token', function(err, activity){
-  console.log(err, activity);
-});
