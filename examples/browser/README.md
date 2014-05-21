da769d12497b487182767603effe4bd89be2ffc7
tiagoalves <tiago.alves@muzzley.com>
2014-05-21 19:39:59 +0100

    Refactored the browser examples.

--- examples/browser/README.md
+++ examples/browser/README.md
@@ -2,14 +2,12 @@
 
 The examples of how to use the Muzzley JS lib in a browser environment can be found in the `html/` folder.
 
-Note that you will have to change the provided application `token`s to your own in the source of the examples. You can get them at http://www.muzzley.com.
-
 If you're used to developing with Node.js, you can easily set up an HTTP server on your local machine that serves the example HTML files.
 
 Follow these steps:
 
 * Clone this repo
-* Browse to this folder in your command line
+* Browse to `examples/browser/server/` in your command line
 * Run `npm install`
 * Run `node server.js`
-* Open http://localhost:3000/simple.html
+* Open http://localhost:3000/app.html
--- examples/browser/server/server.js
+++ examples/browser/server/server.js
@@ -6,25 +6,25 @@
 var fs = require('fs');
 var UglifyJS = require("uglify-js");
 var browserify = require('browserify');
-var version = require('../../package.json');
+var version = require('../../../package.json');
 
 var port = process.env.PORT || 3000;
-var exampleUrl = 'http://localhost:'+port+'/simple.html';
+var exampleUrl = 'http://localhost:'+port+'/app.html';
 
-var js = browserify(['../../vendor/sockjs.js', 'debug']);
-js.require('../../lib/', {expose:'muzzley-client'});
+var js = browserify(['../../../vendor/sockjs.js', 'debug']);
+js.require('../../../lib/', {expose:'muzzley-client'});
 
-js.add('../../lib/dist-browser.js');
+js.add('../../../lib/dist-browser.js');
 
 js.bundle(function(err, file){
   if (err){
-    return console.log('Oops. Something just went wront. Error:', err);
+    return console.log('Oops. Something just went wrong. Error:', err);
   }
 
   var fileName;
   var minified = UglifyJS(file);
 
-  fileName = __dirname + '/lib/muzzley-client-' + version.version + '.js';
+  fileName = __dirname + '/../lib/muzzley-client-' + version.version + '.js';
   fs.writeFileSync(fileName, file);
 
   // Start the server
@@ -45,8 +45,8 @@ var express = require('express');
 var app = express();
 
 app
-  .use(express.static('./html'))
-  .use(express.static('./lib'));
+  .use(express.static('../html'))
+  .use(express.static('../lib'));
 
 
 app.get('/', function (req, res) {
